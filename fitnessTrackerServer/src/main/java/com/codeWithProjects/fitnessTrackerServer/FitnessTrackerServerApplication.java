c77deedce69f59d0319fb9ed25bf48b6c9d99bea
Yuvraj Sharma <yuvrajsharmaa2022@gmail.com>
2025-01-05 23:59:04 +0530

    firness

---
+++ fitnessTrackerServer/src/main/java/com/codeWithProjects/fitnessTrackerServer/FitnessTrackerServerApplication.java
@@ -0,0 +1,13 @@
+package com.codeWithProjects.fitnessTrackerServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessTrackerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessTrackerServerApplication.class, args);
+	}
+
+}
